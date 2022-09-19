85989cd422ce024ae11ba4bb68e5333bfbe8b704
Pavel Sheremet <pashqa.sheremet@gmail.com>
2022-09-19 11:55:40 +0300

    remove package-lock

---
+++ src/api/accordion-module/controllers/accordion-module.js
@@ -0,0 +1,9 @@
+'use strict';
+
+/**
+ * accordion-module controller
+ */
+
+const { createCoreController } = require('@strapi/strapi').factories;
+
+module.exports = createCoreController('api::accordion-module.accordion-module');
---
+++ src/api/accordion-module/routes/accordion-module.js
@@ -0,0 +1,9 @@
+'use strict';
+
+/**
+ * accordion-module router
+ */
+
+const { createCoreRouter } = require('@strapi/strapi').factories;
+
+module.exports = createCoreRouter('api::accordion-module.accordion-module');
