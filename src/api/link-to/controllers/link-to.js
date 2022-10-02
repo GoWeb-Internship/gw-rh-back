c7949026e39361d86875967d89939d51b7813d49
Evgeniya Shvec <82171958+shvec-es@users.noreply.github.com>
2022-10-02 14:17:41 +0300

    add linkto page

---
+++ src/api/link-to/controllers/link-to.js
@@ -0,0 +1,9 @@
+'use strict';
+
+/**
+ * link-to controller
+ */
+
+const { createCoreController } = require('@strapi/strapi').factories;
+
+module.exports = createCoreController('api::link-to.link-to');
---
+++ src/api/link-to/services/link-to.js
@@ -0,0 +1,9 @@
+'use strict';
+
+/**
+ * link-to service
+ */
+
+const { createCoreService } = require('@strapi/strapi').factories;
+
+module.exports = createCoreService('api::link-to.link-to');
